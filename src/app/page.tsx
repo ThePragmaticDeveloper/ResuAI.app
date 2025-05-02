7a00b5cca22f3ef47e152315ab18d78e0c37823f
unknown <olabakareademola@gmail.com>
2025-05-02 21:22:14 +0100

    configure and fetch server-side data from the supabase database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,10 @@
-import prisma from "@/lib/prisma";
 
 
 export default async function Home() {
-  const resumes = await prisma.resume.findMany()
 
   return (
-    <ul>
-      {resumes.map((resume: any) => (
-        <li key={resume.id}>
-        <h2>{resume.title}</h2>
-        <p>{resume.description}</p>
-        </li>
-      ))}
-      </ul>
+    <div>
+      <h1>Hello world</h1>
+    </div>
   );
 }
