d332484b2be2c630683cecb0b84fff998a317e40
unknown <olabakareademola@gmail.com>
2025-05-06 17:09:56 +0100

    remove redundant code from ResumeEditor.tsx

--- src/app/(app)/editor/ResumeEditor.tsx
+++ src/app/(app)/editor/ResumeEditor.tsx
@@ -1,7 +1,5 @@
 "use client"
 
-import { AppSidebar } from "@/components/app-sidebar"
-import ThemeToggle from "@/components/ThemeToggle"
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -9,19 +7,14 @@ import {
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
-import { Button } from "@/components/ui/button"
+} from "@/components/ui/breadcrumb";
 import { Separator } from "@/components/ui/separator"
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
-} from "@/components/ui/sidebar"
-import { ArrowBigLeftDash, LayoutDashboard } from "lucide-react"
-import Link from "next/link"
-import ClerkUserButton from "../ClerkUserButton"
-import GeneralInfoForm from "./forms/GeneralInfoForm"
-import PersonalInfoForm from "./forms/PersonalInfoForm"
+} from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 import { useSearchParams } from "next/navigation"
 import { steps } from "./steps"
 
@@ -66,17 +59,6 @@ export default function ResumeEditor() {
               </BreadcrumbList>
             </Breadcrumb>
           </div>
-
-          <div className='flex items-center gap-4 px-4'>
-           {/* <Button asChild className="rounded-xl bg-gradient" size="sm">
-             <Link href="/dashboard" className="py-5.5 px-5">
-              <ArrowBigLeftDash className="size-[1.3rem] mt-[-.1rem]" strokeWidth={1.2} />
-              <span className="text-lg relative right-1">Dashboard</span>
-             </Link>
-           </Button> */}
-           {/* <ThemeToggle /> */}
-           {/* <ClerkUserButton /> */}
-         </div>
         </header>
         <div className="flex flex-1 gap-4 p-4 pt-0">
           <div className='w-1/3 overflow-y-auto md:w-1/3 p-8.5'>
@@ -84,18 +66,6 @@ export default function ResumeEditor() {
           </div>
           <div className='w-2/3 overflow-y-auto md:w-2/3 p-10'></div>
         </div>
-
-
-        {/* <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-          </div>
-          <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
-        </div> */}
-
-
       </SidebarInset>
     </SidebarProvider>
   )
