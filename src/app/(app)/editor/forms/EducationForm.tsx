22bf516a886ae00aa35581a518ca3913c08d7f9e
unknown <olabakareademola@gmail.com>
2025-05-28 22:21:38 +0100

    implement drag&drop on EducationForms & re-order on ResumePreviewPaper

--- src/app/(app)/editor/forms/EducationForm.tsx
+++ src/app/(app)/editor/forms/EducationForm.tsx
@@ -1,5 +1,23 @@
 "use client"
 
+import {
+  closestCenter,
+  DndContext,
+  DragEndEvent,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import { restrictToVerticalAxis } from "@dnd-kit/modifiers";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  useSortable,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
@@ -9,6 +27,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { GripHorizontal, Plus } from "lucide-react";
 import { useEffect } from "react";
 import { useFieldArray, useForm, UseFormReturn } from "react-hook-form";
+import { cn } from "@/lib/utils";
 
 export default function EducationForm({resumeData, setResumeData}: EditorFormProps) {
   
@@ -29,10 +48,29 @@ export default function EducationForm({resumeData, setResumeData}: EditorFormPro
     return () => subscription.unsubscribe()
   }, [form.watch, setResumeData])
   
-  const { fields, append, remove } = useFieldArray({
+  const { fields, append, remove, move } = useFieldArray({
     control: form.control,
     name: "educations",
   });
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    }),
+  );
+
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event;
+
+    if (over && active.id !== over.id) {
+      const oldIndex = fields.findIndex((field) => field.id === active.id);
+      const newIndex = fields.findIndex((field) => field.id === over.id);
+      move(oldIndex, newIndex);
+      return arrayMove(fields, oldIndex, newIndex);
+    }
+  }
+
 
   return (
     <div className="max-w-xl mx-auto space-y-6">
@@ -48,18 +86,28 @@ export default function EducationForm({resumeData, setResumeData}: EditorFormPro
       </div>
 
       <Form {...form}>
-        <form
-          className="space-y-5"
-          onSubmit={(e) => e.preventDefault()}
-        >
+        <form className="space-y-3">
+          <DndContext
+            sensors={sensors}
+            collisionDetection={closestCenter}
+            onDragEnd={handleDragEnd}
+            modifiers={[restrictToVerticalAxis]}
+          >
+            <SortableContext
+              items={fields}
+              strategy={verticalListSortingStrategy}
+            >
           {fields.map((field, index) => (
             <EducationItem
+              id={field.id}
               key={field.id}
               index={index}
               form={form}
               remove={remove}
             />
           ))}
+        </SortableContext>
+        </DndContext>
           <div className="flex">
           <Button
             type="button"
@@ -85,18 +133,46 @@ export default function EducationForm({resumeData, setResumeData}: EditorFormPro
 }
 
 interface EducationItemProps {
+  id: string;
   form: UseFormReturn<EducationValues>;
   index: number;
   remove: (index: number) => void;
 }
 
 
-function EducationItem({form, index, remove}: EducationItemProps) {
+function EducationItem({id, form, index, remove}: EducationItemProps) {
+
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({ id });
+
+
   return (
-   <div className='space-y-3 border rounded-md bg-background p-3'>
+  //  <div className='space-y-3 border rounded-md bg-background p-3'>
+   <div
+    className={cn(
+        "space-y-3 rounded-md border bg-background p-3",
+        isDragging && "relative z-50 cursor-grab shadow-xl",
+      )}
+      ref={setNodeRef}
+      style={{
+        transform: CSS.Transform.toString(transform),
+        transition,
+      }}
+   >
     <div className='flex justify-between items-center gap-2'>
       <span className=''>Education {index + 1}</span>
-      <GripHorizontal className='size-5 cursor-grab text-muted-foreground' />
+      <GripHorizontal
+        className="size-5 cursor-grab text-muted-foreground focus:outline-none"
+        {...attributes}
+        {...listeners}
+      />
+      {/* <GripHorizontal className='size-5 cursor-grab text-muted-foreground' /> */}
     </div>
     <FormField
      control={form.control}
