b65eb2e4718fd689fd07e387dcd8e8a0a102f61c
unknown <olabakareademola@gmail.com>
2025-06-03 18:21:31 +0100

    add the resumeDataInclude & ResumeServerdata into the types.ts

--- src/app/(app)/editor/page.tsx
+++ src/app/(app)/editor/page.tsx
@@ -1,5 +1,7 @@
 import { Metadata } from 'next'
 import ResumeEditor from './ResumeEditor';
+import { auth } from '@clerk/nextjs/server';
+import prisma from '@/lib/prisma';
 // import ResumeEditor from './ResumeEditor'
 
 export const metadata: Metadata = {
@@ -7,13 +9,29 @@ export const metadata: Metadata = {
   description: 'Design your resume with our easy-to-use editor.',
 }
 
-// app/editor/page.tsx
+interface PageProps {
+  searchParams: Promise<{ resumeId?: string }>;
+}
 
 export const dynamic = "force-dynamic"; // ✅ prevents static optimization
 
 // import ResumeEditor from "./ResumeEditor";
 
-export default function ResumeEditorPage() {
-  return <ResumeEditor />;
+export default async function ResumeEditorPage({ searchParams }: PageProps) {
+  const { resumeId } = await searchParams;
+  const { userId } = await auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  // const resumeToEdit = resumeId
+  //   ? await prisma.resume.findUnique({
+  //       where: { id: resumeId, userId },
+  //       include: resumeDataInclude,
+  //     })
+  //   : null;
+
+  // return <ResumeEditor resumeToEdit={resumeToEdit} />;
 }
 
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,16 @@
+import { Prisma } from "@prisma/client";
 import { ResumeValues } from "./validation";
 
 export interface EditorFormProps {
   resumeData: ResumeValues;
   setResumeData: React.Dispatch<React.SetStateAction<ResumeValues>>;
 }
+
+export const resumeDataInclude = {
+  workExperiences: true,
+  educations: true,
+} satisfies Prisma.ResumeInclude;
+
+export type ResumeServerData = Prisma.ResumeGetPayload<{
+  include: typeof resumeDataInclude;
+}>;
