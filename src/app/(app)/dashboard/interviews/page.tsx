5c082c590fad212d1bd5a630f0e80885e8ce1777
unknown <olabakareademola@gmail.com>
2025-08-09 11:11:41 +0100

    fix the empty page issue with interviews page

--- src/app/(app)/dashboard/interviews/page.tsx
+++ src/app/(app)/dashboard/interviews/page.tsx
@@ -1,11 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { Lock, PhoneCall } from "lucide-react";
 import Image from "next/image";
 
 
 export default function InterviewsPage() {
   
   return (
-    <div>
-      <Image src='/interview.svg' alt="My SVG" width={300} height={300} />
+    <div className="grid place-items-center w-full h-screen">
+      <div className="flex flex-col items-center gap-6">
+      <Image src='/interview.svg' alt="My SVG" width={200} height={200} />
+      <Button className="gap-4" variant="outline">
+        Start Interview <Lock className="w-4 h-4" />
+      </Button>
+      </div>
     </div>
   )
 }
